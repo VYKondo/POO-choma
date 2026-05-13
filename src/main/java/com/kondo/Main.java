02a82136f761b610344adee1de44cb5dd51d3354
Yudi <ra144029@uem.br>
2026-05-13 09:53:21 -0300

    exercicio 1

---
+++ src/main/java/com/kondo/Main.java
@@ -0,0 +1,10 @@
+package com.kondo;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa vinicius = new Pessoa();
+        vinicius.setNome("vinicius");
+        vinicius.setIdade(18);
+        vinicius.exibirDados();
+    }
+}
